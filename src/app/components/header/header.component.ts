fda7222f68743de3e32ec39efd1494d764dce1e6
ElisOre <elias.orellana.31@gmail.com>
2023-01-19 19:29:26 -0300

    Primer botón

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
   title: string = 'Task App';
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleAddTask() {}
 }
